9fddcd5b67cfd90774101685de3a7c9a1f47f64a
이경용 <sleekydz86@naver.com>
2023-04-21 13:13:50 +0900

    스프링환경셋팅
    스프링환경셋팅

---
+++ src/main/kotlin/com/example/fileupload/FileuploadApplication.kt
@@ -0,0 +1,11 @@
+package com.example.fileupload
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FileuploadApplication
+
+fun main(args: Array<String>) {
+	runApplication<FileuploadApplication>(*args)
+}
